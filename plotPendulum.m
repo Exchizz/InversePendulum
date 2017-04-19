992cef0a27298267ce7f0f6d7fb5ed55b01b7bad
Morten Back Nielsen <man@mir-robots.com>
2017-04-19 22:18:41 +0200

    Control torque

--- plotPendulum.m
+++ plotPendulum.m
@@ -10,8 +10,8 @@ M_rod = 1;      % kg
 l_rod = 1;      % Meters
 I_rod = (1/3) * M_rod * l_rod^2;
 
-r_disk = 0.2;
-M_disk = 1;
+r_disk = 0.4; % kg
+M_disk = 0.200; % kg
 I_0 = M_disk * r_disk^2 /2
 I_disk = I_0 + M_disk * l_rod^2;
 
@@ -19,12 +19,18 @@ I = I_rod + I_disk;
 
 figure;
 
+acc = 1000; % rad / sec^2
+
 thetas = [];
 while 1
     f_rod = (M_rod * g*sin(theta));
     f_disk = (M_disk * g*sin(theta));
-    tau = f_rod*l_rod/2 + f_disk*l_rod;
-    thetaAcc = tau/I;
+    tau_g = f_rod*l_rod/2 + f_disk*l_rod
+
+    tau_c = acc * I_0
+    tau = tau_g + tau_c
+
+    thetaAcc = tau/I
     
     thetaVel = thetaVel + thetaAcc*dt;
     theta = theta + thetaVel*dt
@@ -32,7 +38,7 @@ while 1
     plot([0, l_rod*sin(theta)], [0, l_rod*cos(theta)]);
     hold on;
     dot_ratio = 333;
-    plot(l_rod*sin(theta), l_rod*cos(theta),'r', 'MarkerSize', r_disk*dot_ratio)
+    plot(l_rod*sin(theta), l_rod*cos(theta),'r.', 'MarkerSize', r_disk*dot_ratio)
     axis([-2*l_rod 2*l_rod -2*l_rod 2*l_rod]);
     hold off;
 
